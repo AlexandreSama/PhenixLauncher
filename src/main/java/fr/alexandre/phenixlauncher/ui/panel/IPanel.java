abd92d2cdcde41bb9dbe48d606bad073e13ea7dd
Alexandre <alexandrehainy12@gmail.com>
2021-07-01 01:06:54 +0200

    Setup Gradle Project

---
+++ src/main/java/fr/alexandre/phenixlauncher/ui/panel/IPanel.java
@@ -0,0 +1,12 @@
+package fr.alexandre.phenixlauncher.ui.panel;
+
+import fr.alexandre.phenixlauncher.ui.PanelManager;
+import javafx.scene.layout.GridPane;
+
+public interface IPanel {
+    void init(PanelManager panelManager);
+    GridPane getLayout();
+    void onShow();
+    String getName();
+    String getStylesheetPath();
+}
