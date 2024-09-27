7414f40d449051d626e62a38146d1f3d901cda8f
AlexandreSama <alexandrehainy12@gmail.com>
2024-09-27 09:21:12 +0200

    rework main asset js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,223 +1,156 @@
 /* ContentPane */
-const loginContent = document.getElementById('loginContent');
-const waitingContent = document.getElementById('waitingContent');
-const mainContent = document.getElementById('mainContent');
+const elements = {
+    loginContent: document.getElementById('loginContent'),
+    waitingContent: document.getElementById('waitingContent'),
+    mainContent: document.getElementById('mainContent'),
+    closeAppButton: document.getElementById('closeBtn'),
+    reduceAppButton: document.getElementById('minimizeBtn'),
+    loginWithMicrosoft: document.getElementById('loginOptionMicrosoft'),
+    saveRamButton: document.getElementById('saveRam'),
+    openGameFolderButton: document.getElementById('openGameFolder'),
+    playButton: document.getElementById('playButton'),
+    errorBox: document.getElementById('error-box'),
+    errorTitle: document.getElementById('errorTitle'),
+    errorMessage: document.getElementById('errorText'),
+    profilePictureUser: document.getElementById('profilePicture'),
+    username: document.getElementById('username'),
+    ramLabel: document.getElementById('ramLabel'),
+    ramRange: document.getElementById('ramRange'),
+    progressBar: document.getElementById('progress-bar'),
+    progressLabel: document.getElementById('progress-bar-text')
+};
 
-/* Buttons */
-const closeAppButton = document.getElementById('closeBtn');
-const reduceAppButton = document.getElementById('minimizeBtn');
-const loginWithMicrosoft = document.getElementById('loginOptionMicrosoft');
-const saveRamButton = document.getElementById('saveRam');
-const openGameFolderButton = document.getElementById('openGameFolder');
-const playButton = document.getElementById('playButton');
-
-/* Error-Box */
-const errorBox = document.getElementById('error-box');
-const errorTitle = document.getElementById('errorTitle');
-const errorMessage = document.getElementById('errorText');
-
-/* User Infos */
-const profilePictureUser = document.getElementById('profilePicture');
-const username = document.getElementById('username');
-
-/* Ram Range*/
-const ramLabel = document.getElementById('ramLabel');
-const ramRange = document.getElementById('ramRange');
-
-/* Progress */
-const progressBar = document.getElementById('progress-bar');
-const progressLabel = document.getElementById('progress-bar-text');
-
-/* App */
+/* Initialisation de l'application */
 window.app.versionApp().then(res => {
     document.title = `Phenix Launcher | V${res}`;
 });
 
-closeAppButton.addEventListener('click', () => {
+elements.closeAppButton.addEventListener('click', () => {
     window.app.closeApp();
-})
-
-reduceAppButton.addEventListener('click', () => {
-    window.app.reduceApp();
-})
-/* End App part */
-
-/* Login part */
-loginWithMicrosoft.addEventListener('click', () => {
-    window.mc.loginMS();
-    waitingContent.classList.add('fadeIn');
-
-    // Utilisez requestAnimationFrame pour vous assurer que la classe fadeIn est appliquée
-    // et que le navigateur a eu le temps de re-calculer les styles avant de commencer la transition.
-    requestAnimationFrame(() => {
-        waitingContent.classList.add('visible');
-        loginContent.style.display = 'none';
-    });
-})
-/* End Login part */
-
-/* Errors part */
-window.errors.onWeirdToken((__event, data) => {
-    loginContent.classList.add('fadeIn');
-
-    requestAnimationFrame(() => {
-        loginContent.classList.add('visible');
-        waitingContent.style.display = 'none';
-    });
-})
-
-window.errors.onUserCloseMicrosoftFrame((data) => {
-    errorTitle.innerHTML = data.title;
-    errorMessage.innerHTML = data.message;
-    errorBox.style.display = 'flex';
-    loginContent.style.display = 'flex'
-
-    requestAnimationFrame(() => {
-        loginContent.classList.add('visible');
-        waitingContent.style.display = 'none';
-    });
 });
 
-window.errors.onMinecraftNotOwned((__event, data) => {
-    loginContent.classList.add('fadeIn');
+elements.reduceAppButton.addEventListener('click', () => {
+    window.app.reduceApp();
+});
 
-    requestAnimationFrame(() => {
-        loginContent.classList.add('visible');
-        waitingContent.style.display = 'none';
-    });
-})
+/* Gestion de l'authentification Microsoft */
+elements.loginWithMicrosoft.addEventListener('click', () => {
+    window.mc.loginMS();
+    toggleContentVisibility(elements.waitingContent, elements.loginContent);
+});
 
-/* End Errors part */
-
-/* Main part */
-const changeProgress = progress => {
-    progressBar.style.width = `${progress}%`;
+/* Gestion des erreurs */
+const handleError = (errorElement, data, displayElement) => {
+    elements.errorTitle.innerHTML = data.title;
+    elements.errorMessage.innerHTML = data.message;
+    errorElement.style.display = 'flex';
+    toggleContentVisibility(displayElement, elements.waitingContent);
 };
 
+window.errors.onWeirdToken(() => toggleContentVisibility(elements.loginContent, elements.waitingContent));
+window.errors.onUserCloseMicrosoftFrame((data) => handleError(elements.errorBox, data, elements.loginContent));
+window.errors.onMinecraftNotOwned(() => toggleContentVisibility(elements.loginContent, elements.waitingContent));
+
+/* Gestion de l'affichage principal */
 window.mc.onLoginDone((__event, data) => {
     console.log(data);
-    mainContent.classList.add('fadeIn');
-    profilePictureUser.src = `https://minotar.net/avatar/${data[1]}`;
-    username.innerHTML = data[0];
+    elements.profilePictureUser.src = `https://minotar.net/avatar/${data[1]}`;
+    elements.username.innerHTML = data[0];
+    toggleContentVisibility(elements.mainContent, elements.waitingContent);
+});
 
-    requestAnimationFrame(() => {
-        mainContent.classList.add('visible');
-        waitingContent.style.display = 'none';
-    });
-})
-
-playButton.addEventListener('click', () => {
+elements.playButton.addEventListener('click', () => {
     window.mc.launchGame();
-})
+});
 
+/* Gestion des événements Java */
 window.mc.onJavaEvent((status, message) => {
-    switch (status) {
-        case "javaAlreadyInstalled":
-            progressBar.style.width = '100%';
-            progressLabel.innerHTML = message;
-            break;
-        case "javaNotInstalled":
-            progressLabel.innerHTML = message;
-            break;
-        case "javaExtract":
-            progressLabel.innerHTML = message;
-            break;
-        case "javaInstalled":
-            progressBar.style.width = '100%';
-            progressLabel.innerHTML = message;
-            break;
-        default:
-            break;
-    }
+    updateProgress(status, message);
 });
 
-// Pour écouter les événements Forge
+/* Gestion des événements Forge */
 window.mc.onForgeEvent((status, message) => {
-    switch (status) {
-        case "forgeAlreadyDownloaded":
-            progressBar.style.width = '100%';
-            progressLabel.innerHTML = message;
-            break;
-        case "forgeDownloading":
-            progressBar.style.width = '100%';
-            progressLabel.innerHTML = message;
-            break;
-        case "forgeDownloaded":
-            progressBar.style.width = '100%';
-            progressLabel.innerHTML = message;
-            break;
-        case "forgeAndJavaDownloaded":
-            progressBar.style.width = '100%';
-            progressLabel.innerHTML = message;
-            break;
-        case "forgeOrJavaError":
-            progressBar.style.width = '100%';
-            progressBar.style.backgroundColor = 'red';
-            progressLabel.innerHTML = message;
-            break;
-        default:
-            break;
-    }
+    updateProgress(status, message);
 });
 
+/* Gestion des événements Mod */
 window.mc.onModEvent((status, message) => {
     switch (status) {
         case "modsMissing":
-            progressLabel.innerHTML = message;
+            elements.progressLabel.innerHTML = message;
             break;
         case "modsDownloading":
-            console.log(message);
-            let percent = (message[1] / message[2]) * 100;
-            changeProgress(percent)
-            progressLabel.innerHTML = message[0];
-            break;
         case "modsRemoved":
-            percent = (message[1] / message[2]) * 100;
-            changeProgress(percent)
-            progressLabel.innerHTML = message[0];
+            const percent = (message[1] / message[2]) * 100;
+            changeProgress(percent);
+            elements.progressLabel.innerHTML = message[0];
             break;
         case "modsSync":
-            progressBar.style.width = '100%';
-            progressLabel.innerHTML = message;
+            updateProgress(status, message);
             break;
         case "modsError":
-            progressBar.style.width = '100%';
-            progressBar.style.backgroundColor = 'red';
-            progressLabel.innerHTML = message;
+            showErrorProgress(message);
             break;
         default:
             break;
     }
 });
 
+/* Gestion du téléchargement des données */
 window.mc.onDataDownload((__event, data) => {
-    progressLabel.innerHTML = `Téléchargement des ${data.type} : ${data.task} / ${data.total}`;
+    elements.progressLabel.innerHTML = `Téléchargement des ${data.type} : ${data.task} / ${data.total}`;
     const percent = (data.task / data.total) * 100;
     changeProgress(percent);
 });
 
-
-/* End Main part */
-
-/* Params part */
-
-ramRange.addEventListener('input', () => {
-    ramLabel.innerText = 'Ram : ' + ramRange.valueAsNumber + 'Go';
-})
-
-saveRamButton.addEventListener('click', () => {
-    window.mc.saveRam(ramRange.valueAsNumber);
-    //Reste plus qu'a implémenter une petite box pour dire "C'est sauvegardé tkt"
-})
-
-window.mc.getRam().then((ramValue) => {
-    // On affiche la valeur de la ram (5 ou le nombre qu'a choisi l'utilisateur)
-    ramLabel.innerText = 'Ram : ' + ramValue + 'Go';
-    ramRange.value = ramValue;
+/* Gestion de la RAM */
+elements.ramRange.addEventListener('input', () => {
+    elements.ramLabel.innerText = `Ram : ${elements.ramRange.valueAsNumber}Go`;
 });
 
-openGameFolderButton.addEventListener('click', () => {
-    window.app.openGameFolder();
-})
+elements.saveRamButton.addEventListener('click', () => {
+    window.mc.saveRam(elements.ramRange.valueAsNumber);
+    // Ajouter ici un feedback visuel pour informer l'utilisateur de la sauvegarde.
+});
 
-/* End Params part */
+window.mc.getRam().then((ramValue) => {
+    elements.ramLabel.innerText = `Ram : ${ramValue}Go`;
+    elements.ramRange.value = ramValue;
+});
+
+/* Ouverture du dossier de jeu */
+elements.openGameFolderButton.addEventListener('click', () => {
+    window.app.openGameFolder();
+});
+
+/* Fonctions utilitaires */
+
+// Change la largeur de la barre de progression
+const changeProgress = (progress) => {
+    elements.progressBar.style.width = `${progress}%`;
+};
+
+// Met à jour l'affichage de la progression avec différents états
+const updateProgress = (status, message) => {
+    elements.progressBar.style.width = '100%';
+    elements.progressLabel.innerHTML = message;
+    if (status === "forgeOrJavaError" || status === "modsError") {
+        elements.progressBar.style.backgroundColor = 'red';
+    }
+};
+
+// Affiche un message d'erreur dans la barre de progression
+const showErrorProgress = (message) => {
+    elements.progressBar.style.width = '100%';
+    elements.progressBar.style.backgroundColor = 'red';
+    elements.progressLabel.innerHTML = message;
+};
+
+// Basculer la visibilité des contenus
+const toggleContentVisibility = (showElement, hideElement) => {
+    showElement.classList.add('fadeIn');
+    requestAnimationFrame(() => {
+        showElement.classList.add('visible');
+        hideElement.style.display = 'none';
+    });
+};
